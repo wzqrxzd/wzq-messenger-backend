07f6344d31f31e565f281de8c6099aef0919c320
wzqrxzd <wzqrxzd@gmail.com>
2025-12-21 14:41:10 +0000

    refactor login_route

--- routes/login_route.cxx
+++ routes/login_route.cxx
@@ -1,5 +1,6 @@
 #include "login_route.hxx"
 #include "error.hxx"
+#include "types/UserFields.hxx"
 #include "utils.hxx"
 #include <spdlog/spdlog.h>
 #include <fmt/format.h>
@@ -10,29 +11,72 @@ void LoginRoute::setup()
 {
    CROW_ROUTE(app, "/login").methods(crow::HTTPMethod::POST)([this](const crow::request& req){
     return trySafe([&](){
-      auto body_json = crow::json::load(req.body);
+      UserFields user = loadUserData(req);
 
-      std::string username = getJsonField<std::string>(body_json, "username");
-      std::string password = getJsonField<std::string>(body_json, "password");
+      ensureUserExist(user.username.value());
+      verifyPassword(user);
 
-      ConnectionGuard DB(dbHandle);
+      std::string token = auth.generateJWT(user.username.value());
+      int userId = getUserId(user.username.value());
 
-      pqxx::work W(DB.get());
-      pqxx::result R = W.exec_prepared("find_user", username);
-
-      if(R.size() == 0)
-        throw AuthException(AuthError::InvalidCredentials, "wrong username");
-
-      std::string storedHash = R[0]["password_hash"].c_str();
-
-      if (!auth.verifyPassword(storedHash, password))
-        throw AuthException(AuthError::InvalidCredentials, "wrong password");
-
-      std::string token = auth.generateJWT(username);
-      pqxx::result R_user = W.exec_prepared("find_user_by_username", username);
-      int user_id = R_user[0]["id"].as<int>();
-
-      return json_response(200, fmt::format(R"({{"token":"{}","user_id":"{}"}})", token, user_id));
-  });
+      return buildLoginRouteResponse(token, userId); 
+    });
   });
 };
+
+
+int LoginRoute::getUserId(const std::string& username)
+{
+  ConnectionGuard DB(dbHandle);
+
+  pqxx::work worker(DB.get());
+
+  pqxx::result result = worker.exec_prepared("find_user_by_username", username);
+  int userId = result[0]["id"].as<int>();
+
+  return userId;
+}
+
+UserFields LoginRoute::loadUserData(const crow::request& req)
+{
+  auto bodyJson = crow::json::load(req.body);
+
+  UserFields user;
+  user.username = getJsonField<std::string>(bodyJson, "username");
+  user.password = getJsonField<std::string>(bodyJson, "password");
+
+  return user;
+}
+
+void LoginRoute::ensureUserExist(const std::string& username)
+{
+  ConnectionGuard DB(dbHandle);
+
+  pqxx::work W(DB.get());
+  pqxx::result R = W.exec_prepared("find_user", username);
+
+  if(R.size() == 0)
+    throw AuthException(AuthError::InvalidCredentials, "wrong username");
+}
+
+void LoginRoute::verifyPassword(const UserFields& user)
+{
+  ConnectionGuard DB(dbHandle);
+
+  pqxx::work worker(DB.get());
+  pqxx::result result = worker.exec_prepared("find_user", user.username.value());
+
+  std::string storedHash = result[0]["password_hash"].c_str();
+
+  if (!auth.verifyPassword(storedHash, user.password.value()))
+    throw AuthException(AuthError::InvalidCredentials, "wrong password");
+}
+
+inline crow::response LoginRoute::buildLoginRouteResponse(const std::string& token, const int& userId)
+{
+    return json_response(200,
+        fmt::format(R"({{"token":"{}","user_id":"{}"}})",
+          token,
+          userId
+    ));
+}
--- routes/login_route.hxx
+++ routes/login_route.hxx
@@ -3,12 +3,19 @@
 
 #include "route.hxx"
 #include "crow.h"
+#include "types/UserFields.hxx"
 
 class LoginRoute : public Route
 {
   public:
     explicit LoginRoute(crow::App<crow::CORSHandler>& app, AuthService& auth, Database& db);
     void setup() override;
+
+    UserFields loadUserData(const crow::request& req);
+    void ensureUserExist(const std::string& username);
+    void verifyPassword(const UserFields& user);
+    crow::response buildLoginRouteResponse(const std::string& token, const int& userId);
+    int getUserId(const std::string& username);
 };
 
 #endif
